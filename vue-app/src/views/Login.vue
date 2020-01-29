bba07c8256a71924291c89cec258cc31100010cc
nani-ya <13865709339@163.com>
2020-01-29 17:52:42 +0800

    第一次提交

---
+++ vue-app/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+    <h2>login页面</h2>
+    <el-button>危险按钮</el-button>
+  </div>
+</template>
+<script>
+export default {
+  name: "login"
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #34495e;
+}
+h2 {
+  color: #ffffff;
+}
+</style>
